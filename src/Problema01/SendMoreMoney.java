5270b18ee8330c8d2c85b08215b7469a168bbb65
arturSantos <artur.santos@southsystem.com.br>
2023-06-26 19:50:58 -0300

    Updated SEND MORE MONEY 2.0

--- src/Problema01/SendMoreMoney.java
+++ src/Problema01/SendMoreMoney.java
@@ -17,18 +17,18 @@ public class SendMoreMoney {
                                         MORE = (M * 1000) + (O * 100) + (R * 10) + (E);
                                         MONEY = (M * 10000) + (O * 1000) + (N * 100) + (E * 10) + (Y);
 
-                                        if (S != E && S != N && S != D && S != M && S != O && S != R && S != Y)
-                                            if (E != N && E != D && E != M && E != O && E != R && E != Y)
-                                                if (N != D && N != M && N != O && N != R && N != Y)
-                                                    if (D != M && D != O && D != R && D != Y)
-                                                        if (M != O && M != R && M != Y)
-                                                            if (O != R && O != Y)
-                                                                if (SEND + MORE == MONEY) {
-                                                                    System.out.println(" SEND     " + S + E + N + D +
-                                                                            "\n MORE     " + M + O + R + E +
-                                                                            "\n-----    ----- " +
-                                                                            "\nMONEY    " + M + O + N + E + Y + "\n");
-                                                                }
+                                        if ((S != E && S != N && S != D && S != M && S != O && S != R && S != Y)
+                                                && (E != N && E != D && E != M && E != O && E != R && E != Y)
+                                                && (N != D && N != M && N != O && N != R && N != Y)
+                                                && (D != M && D != O && D != R && D != Y)
+                                                && (M != O && M != R && M != Y)
+                                                && (O != R && O != Y)
+                                                && (SEND + MORE == MONEY)) {
+                                            System.out.println("\n SEND     " + S + E + N + D +
+                                                    "\n MORE     " + M + O + R + E +
+                                                    "\n-----    ----- " +
+                                                    "\nMONEY    " + M + O + N + E + Y + "\n");
+                                        }
                                     }
                                 }
                             }

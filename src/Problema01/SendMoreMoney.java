7ebf524d8a362ae9a3f3f8966e07c64a7304ea73
arturSantos <artur.santos@southsystem.com.br>
2023-06-26 19:47:08 -0300

    Updated SEND MORE MONEY

--- src/Problema01/SendMoreMoney.java
+++ src/Problema01/SendMoreMoney.java
@@ -2,34 +2,40 @@ package Problema01;
 
 public class SendMoreMoney {
 
-    private static void generateEightDigitsToChecker(int[] digits, int count) {
-        if (count >= 8) {
-            checkAndPrint(digits[0], digits[1], digits[2], digits[3], digits[4], digits[5], digits[6], digits[7]);
-            return;
-        }
-        outer:
-        for (int digit=0; digit <= 9; digit++) {
-            digits[count] = digit;
-            for (int i = 0; i < count; i++)
-                if (digits[i] == digit) continue outer;
-            generateEightDigitsToChecker(digits, count+1);
-        }
-    }
-
-    private static void checkAndPrint(int s, int e, int n, int d, int m, int o, int r, int y) {
-        int send = (s * 1000) + (e * 100) + (n * 10) + (d);
-        int more = (m * 1000) + (o * 100) + (r * 10) + (e);
-        int money = (m * 10000) + (o * 1000) + (n * 100) + (e * 10) + (y);
-
-        if ((m != 0) && (send + more == money)) {
-            System.out.println(" SEND     " + send);
-            System.out.println(" MORE     " + more);
-            System.out.println("-----    ----- ");
-            System.out.println("MONEY    " + money + "\n");
-        }
-    }
-
     public static void main(String[] args) {
-        generateEightDigitsToChecker(new int[10], 0);
+        int S, E, N, D, M, O, R, Y, SEND, MORE, MONEY;
+
+        for (S = 1; S < 10; S++) {
+            for (E = 0; E < 10; E++) {
+                for (N = 0; N < 10; N++) {
+                    for (D = 0; D < 10; D++) {
+                        for (M = 1; M < 10; M++) {
+                            for (O = 0; O < 10; O++) {
+                                for (R = 0; R < 10; R++) {
+                                    for (Y = 0; Y < 10; Y++) {
+                                        SEND = (S * 1000) + (E * 100) + (N * 10) + (D);
+                                        MORE = (M * 1000) + (O * 100) + (R * 10) + (E);
+                                        MONEY = (M * 10000) + (O * 1000) + (N * 100) + (E * 10) + (Y);
+
+                                        if (S != E && S != N && S != D && S != M && S != O && S != R && S != Y)
+                                            if (E != N && E != D && E != M && E != O && E != R && E != Y)
+                                                if (N != D && N != M && N != O && N != R && N != Y)
+                                                    if (D != M && D != O && D != R && D != Y)
+                                                        if (M != O && M != R && M != Y)
+                                                            if (O != R && O != Y)
+                                                                if (SEND + MORE == MONEY) {
+                                                                    System.out.println(" SEND     " + S + E + N + D +
+                                                                            "\n MORE     " + M + O + R + E +
+                                                                            "\n-----    ----- " +
+                                                                            "\nMONEY    " + M + O + N + E + Y + "\n");
+                                                                }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }

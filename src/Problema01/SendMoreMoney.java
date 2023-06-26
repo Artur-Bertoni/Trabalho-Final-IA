408b4e85afc7b03c0f894fc73986686a439932e3
arturSantos <artur.santos@southsystem.com.br>
2023-06-26 19:54:48 -0300

    Updated SEND MORE MONEY 3.0

--- src/Problema01/SendMoreMoney.java
+++ src/Problema01/SendMoreMoney.java
@@ -3,19 +3,17 @@ package Problema01;
 public class SendMoreMoney {
 
     public static void main(String[] args) {
-        int S, E, N, D, M, O, R, Y, SEND, MORE, MONEY;
-
-        for (S = 1; S < 10; S++) {
-            for (E = 0; E < 10; E++) {
-                for (N = 0; N < 10; N++) {
-                    for (D = 0; D < 10; D++) {
-                        for (M = 1; M < 10; M++) {
-                            for (O = 0; O < 10; O++) {
-                                for (R = 0; R < 10; R++) {
-                                    for (Y = 0; Y < 10; Y++) {
-                                        SEND = (S * 1000) + (E * 100) + (N * 10) + (D);
-                                        MORE = (M * 1000) + (O * 100) + (R * 10) + (E);
-                                        MONEY = (M * 10000) + (O * 1000) + (N * 100) + (E * 10) + (Y);
+        for (int S = 0; S < 10; S++) {
+            for (int E = 0; E < 10; E++) {
+                for (int N = 0; N < 10; N++) {
+                    for (int D = 0; D < 10; D++) {
+                        for (int M = 1; M < 10; M++) {
+                            for (int O = 0; O < 10; O++) {
+                                for (int R = 0; R < 10; R++) {
+                                    for (int Y = 0; Y < 10; Y++) {
+                                        int SEND = (S * 1000) + (E * 100) + (N * 10) + (D);
+                                        int MORE = (M * 1000) + (O * 100) + (R * 10) + (E);
+                                        int MONEY = (M * 10000) + (O * 1000) + (N * 100) + (E * 10) + (Y);
 
                                         if ((S != E && S != N && S != D && S != M && S != O && S != R && S != Y)
                                                 && (E != N && E != D && E != M && E != O && E != R && E != Y)

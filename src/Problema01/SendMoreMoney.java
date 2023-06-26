9e4eb1ef3fb5d09ec7c2f6c7f6233000576b2cdf
arturSantos <artur.santos@southsystem.com.br>
2023-06-26 20:46:06 -0300

    Otimização

--- src/Problema01/SendMoreMoney.java
+++ src/Problema01/SendMoreMoney.java
@@ -5,17 +5,17 @@ public class SendMoreMoney {
     public static void main(String[] args) {
         int count = 0;
 
-        for (int Y = 0; Y < 10; Y++)
-            for (int E = 0; E < 10; E++)
-                for (int N = 0; N < 10; N++)
-                    for (int D = 0; D < 10; D++)
-                        for (int S = 0; S < 10; S++)
+        for (int M = 1; M < 10; M++)
+            for (int S = 0; S < 10; S++)
+                for (int E = 0; E < 10; E++)
+                    for (int N = 0; N < 10; N++)
+                        for (int D = 0; D < 10; D++)
                             for (int O = 0; O < 10; O++)
                                 for (int R = 0; R < 10; R++)
-                                    for (int M = 1; M < 10; M++) {
-                                        int SEND = (S * 1000) + (E * 100) + (N * 10) + (D);
-                                        int MORE = (M * 1000) + (O * 100) + (R * 10) + (E);
-                                        int MONEY = (M * 10000) + (O * 1000) + (N * 100) + (E * 10) + (Y);
+                                    for (int Y = 0; Y < 10; Y++) {
+                                        String SEND = String.valueOf(S) + E + N + D;
+                                        String MORE = String.valueOf(M) + O + R + E;
+                                        String MONEY = String.valueOf(M) + O + N + E + Y;
 
                                         count++;
 
@@ -25,17 +25,13 @@ public class SendMoreMoney {
                                                 && (D != M && D != O && D != R && D != Y)
                                                 && (M != O && M != R && M != Y)
                                                 && (O != R && O != Y)
-                                                && (SEND + MORE == MONEY))
-
-                                            //86 103 083
-                                            //5 679 083
-                                            //23 111 173
+                                                && (Integer.parseInt(SEND) + Integer.parseInt(MORE) == Integer.parseInt(MONEY)))
 
                                             System.out.println("\n SEND     " + SEND +
                                                     "\n MORE     " + MORE +
                                                     "\n-----    ----- " +
                                                     "\nMONEY    " + MONEY + "\n\n" +
-                                                    "Iterações: " + count + "\n");
+                                                    "Número de Iterações: " + count + "\n");
                                     }
     }
 }
